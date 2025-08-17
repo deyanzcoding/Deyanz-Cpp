25fee7ae802aaf28c66cb32bf7ae2a61ba85af24
Deyanz Coding <deyanzcoding@gmail.com>
2025-08-17 11:08:47 -0700

    cmd in cpp
    i use cmd commandz in cpp program as experiment.

---
+++ CMD in c++/show wifi password.cpp
@@ -0,0 +1,15 @@
+
+#include<iostream>
+#include<stdlib.h>
+using namespace std;
+
+int main(){
+
+system("color 0a");
+
+system("netsh wlan show profile");
+
+
+
+return 0;
+}
---
+++ CMD in c++/systeminfo.cpp
@@ -0,0 +1,12 @@
+#include<stdlib.h>
+#include<iostream>
+using namespace std;
+
+int main(){
+system("color 0a");
+system("systeminfo");
+
+
+
+return 0;
+}
